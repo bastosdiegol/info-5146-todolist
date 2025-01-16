bdeaaee28fb908cc33c8cbf0520735d856fc9bd4
Diego Bastos <bastosdiegol@gmail.com>
2025-01-16 14:46:02 -0500

    fixed github pages wpa service worker reference

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,24 @@
-const CACHE_NAME = 'to-do-pwa-cache-v1';
+const CACHE_NAME = "to-do-pwa-cache-v1";
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/app.js',
-    '/manifest.json',
-    '/icons/icon-128.png',
-    '/icons/icon-512.png'
+  "/info-5146-todolist/",
+  "/info-5146-todolist/index.html",
+  "/info-5146-todolist/style.css",
+  "/info-5146-todolist/app.js",
+  "/info-5146-todolist/manifest.json",
+  "/info-5146-todolist/icons/icon-128.png",
+  "/info-5146-todolist/icons/icon-512.png",
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => response || fetch(event.request))
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
